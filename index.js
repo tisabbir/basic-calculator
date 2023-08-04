19a23ff5f5f1239c60090e86ea1a17bc010ea4e2
MD Tarekul Islam Sabbir <tisabbir0@gmail.com>
2023-08-04 11:22:01 +0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var num1 = prompt("Enter your first number: ");
+var num2 = prompt("Enter your second number: ");
+
+// var num1 = 20;
+// var num2 = 10;
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+//addition 
+result = num1 + num2;
+document.write("Addition: " + result + "<br/>");
+
+//dividation
+result = num1 - num2;
+document.write("Substraction: " + result  + "<br/>");
+
+//multiplication
+result = num1 * num2;
+document.write("Multiflication: " + result  + "<br/>");
+
+//dividation
+result = num1 / num2;
+document.write("Dividation: " + result  + "<br/>");
+
+//Extraction
+result = num1 % num2;
+document.write("Extraction: " + result  + "<br/>");
+
